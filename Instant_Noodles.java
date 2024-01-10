3420b2e511b767ba9c99016239be7ef52f76fc72
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-01-10 11:14:55 +0530

    import java.io.*; import java.util.*;
    public class Sphere{
        public static void main(String[] args){
            Scanner obj = new Scanner(System.in);
            
            int a = obj.nextInt();
            int b = obj.nextInt();
            
            int result = a * b;
            System.out.println(result);
            
        }
    }

---
+++ Instant_Noodles.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+public class Sphere{
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        
+        int result = a * b;
+        System.out.println(result);
+        
+    }
+}
