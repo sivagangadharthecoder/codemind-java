2d7ccc0efefd8d7347211ee2768edfcab73932e6
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-01-24 10:55:51 +0530

    import java.util.*;
    public class Perimeter{
        public static void main(String args[]){
            Scanner obj = new Scanner(System.in);
            
            int n1 = obj.nextInt();
            int n2 = obj.nextInt();
            int result = n1 * n2;
            
            System.out.print(result);
        }
    }

---
+++ Multiplication.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Perimeter{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        
+        int n1 = obj.nextInt();
+        int n2 = obj.nextInt();
+        int result = n1 * n2;
+        
+        System.out.print(result);
+    }
+}
