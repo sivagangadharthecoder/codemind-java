3346fc241af1c4968bc4de9c6bd50854e1471fcf
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-01-24 10:59:54 +0530

    import java.util.*;
    public class Perimeter{
        public static void main(String args[]){
            Scanner obj = new Scanner(System.in);
            
            int a = obj.nextInt();
            int b = obj.nextInt();
            
            System.out.print(a/b);
        }
    }

---
+++ Division.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Perimeter{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        
+        System.out.print(a/b);
+    }
+}
