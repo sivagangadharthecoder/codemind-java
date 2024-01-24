675fd822eda82317975a90a957d592dfa2d32f7e
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-01-24 11:40:57 +0530

    import java.util.*;
    public class Perimeter{
        public static void main(String args[]){
            Scanner obj = new Scanner(System.in);
            
            int n = obj.nextInt();
            int m = obj.nextInt();
            
            int t = (n * 5) + (m * 7);
            System.out.print(t);
        }
    }

---
+++ King_Tours_.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Perimeter{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        
+        int n = obj.nextInt();
+        int m = obj.nextInt();
+        
+        int t = (n * 5) + (m * 7);
+        System.out.print(t);
+    }
+}
