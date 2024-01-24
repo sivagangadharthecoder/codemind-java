fb3865fd75054ea7c2fb54f3788c9325928f7450
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-01-24 11:36:11 +0530

    import java.util.*;
    public class Perimeter{
        public static void main(String args[]){
            Scanner obj = new Scanner(System.in);
            
            float a = obj.nextFloat();
            float b = obj.nextFloat();
            
            System.out.format("%.2f", a  * b);
        }
    }

---
+++ Program_to_Multiply_Two_Floating-Point_Numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Perimeter{
+    public static void main(String args[]){
+        Scanner obj = new Scanner(System.in);
+        
+        float a = obj.nextFloat();
+        float b = obj.nextFloat();
+        
+        System.out.format("%.2f", a  * b);
+    }
+}
