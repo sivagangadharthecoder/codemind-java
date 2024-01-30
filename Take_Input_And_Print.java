f70053fd46e409353bd1081a12863d15ee7bbcf2
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-01-30 09:47:04 +0530

    import java.util.*;
    public class str{
        public static void main(String[] args){
            Scanner obj = new Scanner(System.in);
            
            String s = obj.nextLine();
            System.out.println(s);
        }
    }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class str{
+    public static void main(String[] args){
+        Scanner obj = new Scanner(System.in);
+        
+        String s = obj.nextLine();
+        System.out.println(s);
+    }
+}
