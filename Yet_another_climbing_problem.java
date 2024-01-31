1a6bc450f504c870c33d6ba9066f5bde269d02bf
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-01-31 09:49:04 +0530

    import java.util.*;
    public class Climb{
    public static void main(String[] args){
        Scanner obj = new Scanner(System.in);
        
        int t = obj.nextInt();
        for(int i = 0; i < t; i++){
        int a = obj.nextInt();
        int b = obj.nextInt();
        
        int q = a / b;
        int r = a % b;
        System.out.println(q + r);
        
        }
    }
    }

--- Yet_another_climbing_problem.java
+++ Yet_another_climbing_problem.java
@@ -1,18 +1,18 @@
 import java.util.*;
 
-public class rp{
-    public static void main(String args[]){
-        Scanner obj = new Scanner(System.in);
-        
-        int t = obj.nextInt();
-        for(int itr=0; itr< t; itr++){
-            int a = obj.nextInt();
-            int b = obj.nextInt();
-            
-            int q = a / b;
-            int r = a % b;
-            System.out.println(q+r);
-            
-        }
+public class Climb{
+public static void main(String[] args){
+    Scanner obj = new Scanner(System.in);
+    
+    int t = obj.nextInt();
+    for(int i = 0; i < t; i++){
+    int a = obj.nextInt();
+    int b = obj.nextInt();
+    
+    int q = a / b;
+    int r = a % b;
+    System.out.println(q + r);
+    
     }
+}
 }
