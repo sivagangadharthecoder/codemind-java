ca2d14a4e2a2a81b398c5610f9bf5ceef28f62f4
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-01 21:57:49 +0530

    import java.util.Scanner;
    public class OctalDecimal{
        public static void main(String[] args) {
            Scanner obj = new Scanner(System.in);
            
            int a = obj.nextInt();
            int b = obj.nextInt();
            int c = obj.nextInt();
            
            double s = (a + b + c)/2.0;
            
            double area = Math.sqrt(s * (s-a)*(s-b)*(s-c));
            
            System.out.printf("%.2f", area);
            
        }
    }

---
+++ Area_of_Triangle_.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class OctalDecimal{
+    public static void main(String[] args) {
+        Scanner obj = new Scanner(System.in);
+        
+        int a = obj.nextInt();
+        int b = obj.nextInt();
+        int c = obj.nextInt();
+        
+        double s = (a + b + c)/2.0;
+        
+        double area = Math.sqrt(s * (s-a)*(s-b)*(s-c));
+        
+        System.out.printf("%.2f", area);
+        
+    }
+}
+
