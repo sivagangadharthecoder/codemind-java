21fc1ca97cfb665d5457f18424c5ac7177235976
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-06 11:52:49 +0530

    import java.util.*;
    public class Height
    {
    	public static void main(String[] args)
    	{
    	    Scanner sc = new Scanner(System.in);
    	    
    	    int n = sc.nextInt();
    	    int d = sc.nextInt();
    	    
    	    if(n > d)
    	    {
    	    System.out.println(n);
    	    }
    	    else{
    	        System.out.println(d);
    	    }
    }
    }

---
+++ Height.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+public class Height
+{
+	public static void main(String[] args)
+	{
+	    Scanner sc = new Scanner(System.in);
+	    
+	    int n = sc.nextInt();
+	    int d = sc.nextInt();
+	    
+	    if(n > d)
+	    {
+	    System.out.println(n);
+	    }
+	    else{
+	        System.out.println(d);
+	    }
+}
+}
