8675d9ce305d78fbab8940c9611d4ef7aebb2eb1
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-07 19:52:11 +0530

    import java.util.*;
    public class Tickets{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int a = sc.nextInt() , b = sc.nextInt(), c = sc.nextInt();
            
            int result = Math.max(Math.max(a,b) , c);
            System.out.println(result);
        }
    }

---
+++ Largest_number_among_3_numbers.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Tickets{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt() , b = sc.nextInt(), c = sc.nextInt();
+        
+        int result = Math.max(Math.max(a,b) , c);
+        System.out.println(result);
+    }
+}
