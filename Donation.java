13f6f47ab62b450a04c898150a159ece4e193110
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-07 10:17:35 +0530

    import java.util.*;
    public class Donation{
        public static void main(String[] args)
        {
            Scanner sc = new Scanner(System.in);
            
            int x = sc.nextInt();
            int y = sc.nextInt();
            
            System.out.println(y - x);
        }
    }

---
+++ Donation.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class Donation{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+        
+        System.out.println(y - x);
+    }
+}
