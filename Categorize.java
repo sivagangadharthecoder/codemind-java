d3278698078371076316b004e64e8f02a4442e36
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-07 20:33:53 +0530

    import java.util.*;
    public class Height{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            double x = sc.nextDouble();
            
            if(x<150) {
                System.out.println("Person is Dwarf.");
            }
            else if(x>50 && x<=165)
            {
                System.out.println("Person is average heighted.");
            }
            else if(x>165 && x<=195)
            {
                System.out.println("Person is taller.");
            }
            else{
                System.out.println("Abnormal height.");
            }
        }
    }

---
+++ Categorize.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class Height{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        double x = sc.nextDouble();
+        
+        if(x<150) {
+            System.out.println("Person is Dwarf.");
+        }
+        else if(x>50 && x<=165)
+        {
+            System.out.println("Person is average heighted.");
+        }
+        else if(x>165 && x<=195)
+        {
+            System.out.println("Person is taller.");
+        }
+        else{
+            System.out.println("Abnormal height.");
+        }
+    }
+}
