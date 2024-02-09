c307fa609da84fc0f2af507c5722f28d504f6a7a
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-09 22:48:40 +0530

    import java.util.*;
    public class Nto1{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int num = sc.nextInt();
            
            for(int i=num; i>=1; i--){
                System.out.print(i + " ");
            }
            
        }
    }

---
+++ N_to_1.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class Nto1{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int num = sc.nextInt();
+        
+        for(int i=num; i>=1; i--){
+            System.out.print(i + " ");
+        }
+        
+    }
+}
