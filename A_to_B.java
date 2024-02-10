db1a02a672856d1e22a289c9421c6aa3cbc52846
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-10 18:36:30 +0530

    import java.util.*;
    public class AB{
        public static void main(String[] args)
        {
            Scanner sc = new Scanner(System.in);
            
            int a = sc.nextInt(), b=sc.nextInt();
            
            for(int i=a; i<=b; i++)
            {
                System.out.print(i+" ");
            }
        }
    }

---
+++ A_to_B.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class AB{
+    public static void main(String[] args)
+    {
+        Scanner sc = new Scanner(System.in);
+        
+        int a = sc.nextInt(), b=sc.nextInt();
+        
+        for(int i=a; i<=b; i++)
+        {
+            System.out.print(i+" ");
+        }
+    }
+}
