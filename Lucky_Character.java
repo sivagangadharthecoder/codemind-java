22d831ce84445f972ef8a36f1572c99e0262ed02
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-11 00:30:29 +0530

    import java.util.*;
    public class LuckyChar{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            String str = sc.nextLine();
            System.out.print(str.charAt(6));
        }
    }

---
+++ Lucky_Character.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class LuckyChar{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        System.out.print(str.charAt(6));
+    }
+}
