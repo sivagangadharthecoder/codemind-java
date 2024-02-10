ce90f2b865e97716ca58789af4b47a27662f86fe
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-10 23:49:26 +0530

    import java.util.*;
    public class NextPrimePalindrome{
        
        public static boolean isPrime(int x){
            int count = 0;
            for(int i=2; i<x; i++)
            {
                if(x % i == 0){
                    count++;
                    break;
                }
            }
            if(count == 0){
                return true;
            }
            else {
                return false;
            }
        }
        
        public static boolean isPalindrome(int y){
            int temp = y;
            int sum = 0;
            while(temp > 0){
                int rem = temp % 10;
                sum = sum*10 + rem;
                temp = temp / 10;
            }
            
            if(sum == y){
                return true;
            }
            else{
                return false;
            }
            
        }
        
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int num = sc.nextInt();
            
            for(int i=num+1; i>0; i++)
            {
                if(isPalindrome(i))
                {
                    if(isPrime(i)){
                        System.out.print(i);
                        break;
                    }
                }
            }
        }
    }

--- Program_to_find_out_the_next_prime_palindrome_number.java
+++ Program_to_find_out_the_next_prime_palindrome_number.java
@@ -1,36 +1,40 @@
 import java.util.*;
 
-public class NextPalindrome{
+public class NextPrimePalindrome{
     
-    public static boolean IsPalin(int x){
-        int temp = x, rev = 0;
-        while(x > 0)
+    public static boolean isPrime(int x){
+        int count = 0;
+        for(int i=2; i<x; i++)
         {
-            int rem = x % 10;
-            rev = rev * 10 + rem;
-            x = x / 10;
-        }
-        return (temp == rev);
-    }
-    
-    public static boolean IsPrime(int y){
-        boolean flag = true;
-        
-        for(int i=2; i<y; i++)
-        {
-            if(y % i == 0)
-            {
-                flag = false;
+            if(x % i == 0){
+                count++;
                 break;
             }
         }
-        if(flag == true)
-        {
+        if(count == 0){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+    
+    public static boolean isPalindrome(int y){
+        int temp = y;
+        int sum = 0;
+        while(temp > 0){
+            int rem = temp % 10;
+            sum = sum*10 + rem;
+            temp = temp / 10;
+        }
+        
+        if(sum == y){
             return true;
         }
         else{
             return false;
         }
+        
     }
     
     public static void main(String[] args){
@@ -38,19 +42,15 @@ public class NextPalindrome{
         
         int num = sc.nextInt();
         
-        for(int i = num+1; i>0; i++){
-        if(IsPalin(i)){
-            if(IsPrime(i)){
-                System.out.println(i);
-            break;
+        for(int i=num+1; i>0; i++)
+        {
+            if(isPalindrome(i))
+            {
+                if(isPrime(i)){
+                    System.out.print(i);
+                    break;
+                }
             }
-            else{
-                continue;
-            }
-        }
-        else{
-            continue;
-        }
         }
     }
 }
