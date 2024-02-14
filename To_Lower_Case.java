53995e2850dd939536acc4ed668c7c62dca53345
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-14 22:21:48 +0530

    import java.util.Scanner;
    public class FirstUniqueCharIndex {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
            String a = sc.nextLine();
            String b = a.toLowerCase();
            System.out.print(b);
        }
    }

---
+++ To_Lower_Case.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class FirstUniqueCharIndex {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String a = sc.nextLine();
+        String b = a.toLowerCase();
+        System.out.print(b);
+    }
+}
