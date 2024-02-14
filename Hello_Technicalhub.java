e4307bccca3cb3a7fbea9dcea2069f822c4d4b91
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-14 21:20:55 +0530

    import java.util.*;
    public class THub{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            String str = sc.nextLine();
            
            System.out.println("Hello Technicalhub");
            System.out.println(str);
        }
    }

---
+++ Hello_Technicalhub.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class THub{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        
+        System.out.println("Hello Technicalhub");
+        System.out.println(str);
+    }
+}
