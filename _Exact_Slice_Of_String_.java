d957853c26d468aee75c95dd4e154fc66e263b03
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-14 21:18:48 +0530

    import java.util.*;
    public class Slicing{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            String str = sc.nextLine();
            int a = sc.nextInt();
            int b = sc.nextInt();
            
            for(int i=a; i<=b; i++)
            {
                System.out.print(str.charAt(i));
            }
        }
    }

--- _Exact_Slice_Of_String_.java
+++ _Exact_Slice_Of_String_.java
@@ -1,15 +1,16 @@
 import java.util.*;
 
-public class Slicing {
-    public static void main(String[] args) {
+public class Slicing{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
         String str = sc.nextLine();
         int a = sc.nextInt();
         int b = sc.nextInt();
         
-        String slicedStr = str.substring(a, b + 1);
-        System.out.println(slicedStr);
+        for(int i=a; i<=b; i++)
+        {
+            System.out.print(str.charAt(i));
         }
     }
 }
