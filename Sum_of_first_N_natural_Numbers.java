3238b7c0ca4f7357bbb07d5f334787e1415482b6
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-15 18:59:18 +0530

    import java.util.*;
    public class Nnatural{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int num = sc.nextInt();
            int sum = 0, i=1;
            while(i <= num){
                sum = sum + i;
                i++;
            }
            System.out.print(sum);
        }
    }

--- Sum_of_first_N_natural_Numbers.java
+++ Sum_of_first_N_natural_Numbers.java
@@ -1,18 +1,16 @@
 import java.util.*;
 
-public class Nsum{
+
+public class Nnatural{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
         int num = sc.nextInt();
-        
-        int sum = 0;
-        for(int i=1; i<=num; i++)
-        {
-            sum+=i;
+        int sum = 0, i=1;
+        while(i <= num){
+            sum = sum + i;
+            i++;
         }
-        
-        System.out.println(sum);
-        
+        System.out.print(sum);
     }
 }
