d6afc7a171a2f3da5dc81172df1b0356f0a22602
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-15 11:46:20 +0530

    import java.util.*;
    public class Max{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            String str = sc.nextLine();
            
            char max = str.charAt(0);
            for(int i=0; i<str.length(); i++){
                if(str.charAt(i)>max)
                {
                    max = str.charAt(i);
                }
            }
            System.out.print(max);
        }
    }

---
+++ Max_of_String.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+public class Max{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        String str = sc.nextLine();
+        
+        char max = str.charAt(0);
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i)>max)
+            {
+                max = str.charAt(i);
+            }
+        }
+        System.out.print(max);
+    }
+}
