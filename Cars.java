0e0fb22bff271f1341f20253c978cacf5ce1d2f1
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-16 11:23:43 +0530

    import java.util.*;
    public class Cars{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int n1 = sc.nextInt();
            int n2 = sc.nextInt();
            int x = sc.nextInt();
            
            int count = 0;
            if(n1 >= n2){
                System.out.print(-1);
            }
            else{
                int temp = 0;
                while(temp <= x){
                    x += n1;
                    temp += n2;
                    count++;
                }
                System.out.println(count);
            }
            
        }
    }

--- Cars.java
+++ Cars.java
@@ -1,25 +1,26 @@
-import java.util.Scanner;
+import java.util.*;
 
-public class CarRacing {
-    public static void main(String[] args) {
+public class Cars{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-
+        
         int n1 = sc.nextInt();
         int n2 = sc.nextInt();
         int x = sc.nextInt();
         
         int count = 0;
-        
-        if(n1 >= n2) {
-            System.out.println(-1);
-        } 
-        else {
-            for (int temp = 0; temp <= x; temp += n2) {
+        if(n1 >= n2){
+            System.out.print(-1);
+        }
+        else{
+            int temp = 0;
+            while(temp <= x){
                 x += n1;
+                temp += n2;
                 count++;
             }
             System.out.println(count);
         }
-
+        
     }
 }
