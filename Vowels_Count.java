e31d64e7d163dd73b97b950b6e85f1e1fdd15009
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-02-16 10:34:19 +0530

    import java.util.*;
    public class CountVowels{
        
        public static boolean isVowel(char ch){
            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
            {
                return true;
            }
            return false;
        }
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            String str = sc.nextLine();
            int count = 0;
            str = str.toLowerCase();
            for(int i=0; i<str.length(); i++){
                if(isVowel(str.charAt(i)))
                {
                    count++;
                }
            }
            
            System.out.print(count);
            
        }
    }

--- Vowels_Count.java
+++ Vowels_Count.java
@@ -1,19 +1,28 @@
 import java.util.*;
 
-public class VowelsCount{
+public class CountVowels{
+    
+    public static boolean isVowel(char ch){
+        if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u')
+        {
+            return true;
+        }
+        return false;
+    }
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
         String str = sc.nextLine();
-        
+        int count = 0;
         str = str.toLowerCase();
-        int vowelCount = 0;
         for(int i=0; i<str.length(); i++){
-            char ch = str.charAt(i);
-            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
-                vowelCount++;
+            if(isVowel(str.charAt(i)))
+            {
+                count++;
             }
         }
-        System.out.print(vowelCount);
+        
+        System.out.print(count);
+        
     }
 }
