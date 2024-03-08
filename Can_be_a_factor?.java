218aafae9fb181c6eb21a8b441d82b6375188d11
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-03-08 16:20:10 +0530

    import java.util.*;
    public class Factor{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int t = sc.nextInt();
            for(int i=0; i<t; i++){
                int a = sc.nextInt(), b = sc.nextInt();
                
                int difference = b - a;
                if(a <= difference || a==b){
                    System.out.println("YES");
                }
                else{
                    System.out.println("NO");
                }
                
                }
            }
        }

--- Can_be_a_factor?.java
+++ Can_be_a_factor?.java
@@ -1,15 +1,21 @@
 import java.util.*;
-public class MyClass {
-    public static void main(String[] args) {
+
+public class Factor{
+    public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
+        
         int t = sc.nextInt();
-        while(t-- > 0) {
-            int a = sc.nextInt();
-            int b = sc.nextInt();
+        for(int i=0; i<t; i++){
+            int a = sc.nextInt(), b = sc.nextInt();
             
-            int z = b - a;
-            if(z >= a || a == b) System.out.println("YES");
-            else System.out.println("NO");
+            int difference = b - a;
+            if(a <= difference || a==b){
+                System.out.println("YES");
+            }
+            else{
+                System.out.println("NO");
+            }
+            
+            }
         }
     }
-} 
