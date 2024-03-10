927df768298cd9ca9999cb8e1d1a156621c7c8a8
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-03-10 14:05:30 +0530

    import java.util.*;
    public class Climbing{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int n = sc.nextInt();
            
            if(n <= 2){
                System.out.println(n);
            }
            else{
                int a = 1;
                int b = 2;
                int sum = 0;
                for(int i=3; i<=n; i++){
                    sum = a+b;
                    a = b;
                    b = sum;
                }
                System.out.print(sum);
            }
        }
    }

--- Climbing_Stairs.java
+++ Climbing_Stairs.java
@@ -1,28 +1,24 @@
 import java.util.*;
 
-public class Solution
-{
-    public static int climbStairs(int n)
-    {
-        if(n <= 2)
-            return n;
-
-        int a = 1;
-        int b = 2;
-
-		for(int i = 3; i <= n; i++)
-		{
-            int sum = a + b;
-            a = b;
-            b = sum;
-        }
-        return b;
-    }
+public class Climbing{
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
-        int num = sc.nextInt();
+        int n = sc.nextInt();
         
-        System.out.print(climbStairs(num));
-    } 
+        if(n <= 2){
+            System.out.println(n);
+        }
+        else{
+            int a = 1;
+            int b = 2;
+            int sum = 0;
+            for(int i=3; i<=n; i++){
+                sum = a+b;
+                a = b;
+                b = sum;
+            }
+            System.out.print(sum);
+        }
+    }
 }
