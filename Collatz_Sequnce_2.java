218b1d78af48d4e4b8f83667adff3ac091014874
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-03-10 10:26:52 +0530

    import java.util.*;
    public class Sequence {
        
        private static int toOne(int n){
            int count = 0;
           while(n > 1){
               if(n % 2 == 0){
                   n = n / 2;
               }
               else{
                   n = 3 * n + 1;
               }
               count++;
           }
           return count;
        }
        
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int a = sc.nextInt();
            int b = sc.nextInt();
            
            int maxcount = 0, maxnumber = 0;
            for(int i=a; i<=b; i++){
                if(toOne(i) > maxcount){
                    maxcount = toOne(i);
                    maxnumber = i;
                }
            }
            System.out.println(maxnumber);
        }
    }

--- Collatz_Sequnce_2.java
+++ Collatz_Sequnce_2.java
@@ -1,35 +1,34 @@
 import java.util.*;
 
-public class Series{
+public class Sequence {
+    
+    private static int toOne(int n){
+        int count = 0;
+       while(n > 1){
+           if(n % 2 == 0){
+               n = n / 2;
+           }
+           else{
+               n = 3 * n + 1;
+           }
+           count++;
+       }
+       return count;
+    }
+    
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
         
         int a = sc.nextInt();
         int b = sc.nextInt();
         
-        int maxNumber = 0;
-        int maxCount = 0;
-        
-        for(int i=a; i<=b; i++)
-        {
-            int count = 0;
-            int num = i;
-            while(num != 1){
-                if(num % 2 == 0){
-                    num = num / 2;
-                }
-                else{
-                    num = 3*num + 1;
-                }
-                count++;
-            }
-            
-            if(count > maxCount){
-                maxCount = count;
-                maxNumber = i;
+        int maxcount = 0, maxnumber = 0;
+        for(int i=a; i<=b; i++){
+            if(toOne(i) > maxcount){
+                maxcount = toOne(i);
+                maxnumber = i;
             }
         }
-        
-        System.out.print(maxNumber);
+        System.out.println(maxnumber);
     }
 }
