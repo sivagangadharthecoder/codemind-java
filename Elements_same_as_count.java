0f4982fa55752e38eda5e550d98c82334386a9ae
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-03-13 17:42:32 +0530

    import java.util.Scanner;
    public class SameCount {
    
        private static int count(int a[], int n) {
            int count = 0;
            for (int i = 0; i < a.length; i++) {
                if (a[i] == n) {
                    count++;
                }
            }
            return count;
        }
    
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
    
            int size = sc.nextInt();
            int[] arr = new int[size];
            for (int i = 0; i < size; i++) {
                arr[i] = sc.nextInt();
            }
    
            StringBuilder sb = new StringBuilder();
            boolean flag = false;
            for (int i = 0; i < arr.length; i++) {
                int element = arr[i];
                int elementCount = count(arr, element);
                if (element == elementCount && sb.indexOf(String.valueOf(arr[i])) == -1) {
                    sb.append(arr[i]+" ");
                    flag = true;
                }
            }
    
            if (!flag) {
                System.out.print(-1);
            } else {
                System.out.print(sb.toString());
            }
        }
    }

---
+++ Elements_same_as_count.java
@@ -0,0 +1,41 @@
+import java.util.Scanner;
+
+public class SameCount {
+
+    private static int count(int a[], int n) {
+        int count = 0;
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] == n) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int[] arr = new int[size];
+        for (int i = 0; i < size; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        boolean flag = false;
+        for (int i = 0; i < arr.length; i++) {
+            int element = arr[i];
+            int elementCount = count(arr, element);
+            if (element == elementCount && sb.indexOf(String.valueOf(arr[i])) == -1) {
+                sb.append(arr[i]+" ");
+                flag = true;
+            }
+        }
+
+        if (!flag) {
+            System.out.print(-1);
+        } else {
+            System.out.print(sb.toString());
+        }
+    }
+}
