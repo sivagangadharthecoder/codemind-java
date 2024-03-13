14614cbbf04842feeeeb5185d23011ffb1792685
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-03-13 11:03:58 +0530

    import java.util.*;
    public class Main {
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
    
            int t = sc.nextInt();
            sc.nextLine(); 
    
            while (t-- > 0) {
                int length = sc.nextInt();
                sc.nextLine(); 
                String str = sc.nextLine();
    
                int count1 = 0;
                for (int i = 0; i < str.length(); i++) {
                    if (str.charAt(i) == '1') {
                        for (int j = i + 1; j < str.length(); j++) {
                            if (str.charAt(j) == '1') {
                                count1++;
                            }
                        }
                    }
                }
                
                int count2 = 0;
                for(int i=0; i<str.length(); i++){
                    if(str.charAt(i) == '1'){
                        count2++;
                    }
                }
                System.out.println(count1 + count2 );
            }
            sc.close();
        }
    }

---
+++ Count_Substrings.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int t = sc.nextInt();
+        sc.nextLine(); 
+
+        while (t-- > 0) {
+            int length = sc.nextInt();
+            sc.nextLine(); 
+            String str = sc.nextLine();
+
+            int count1 = 0;
+            for (int i = 0; i < str.length(); i++) {
+                if (str.charAt(i) == '1') {
+                    for (int j = i + 1; j < str.length(); j++) {
+                        if (str.charAt(j) == '1') {
+                            count1++;
+                        }
+                    }
+                }
+            }
+            
+            int count2 = 0;
+            for(int i=0; i<str.length(); i++){
+                if(str.charAt(i) == '1'){
+                    count2++;
+                }
+            }
+            System.out.println(count1 + count2 );
+        }
+        sc.close();
+    }
+}
