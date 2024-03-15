d5b4053b5b670e63b5ba4d40457c376b29c9002a
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-03-15 23:12:12 +0530

    import java.util.*;
    public class Average{
        public static void main(String[] args) {
            Scanner sc = new Scanner(System.in);
    
            int size = sc.nextInt();
            int A[] = new int[size];
            for (int i = 0; i < size; i++) {
                A[i] = sc.nextInt();
            }
            
            Arrays.sort(A);
            System.out.print(A[size-1]);
        }
    }

---
+++ Maximum_of_an_Array.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Average{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int size = sc.nextInt();
+        int A[] = new int[size];
+        for (int i = 0; i < size; i++) {
+            A[i] = sc.nextInt();
+        }
+        
+        Arrays.sort(A);
+        System.out.print(A[size-1]);
+    }
+}
