01e43cf70c3ec4b50ba3bbf7ac9857a9f5be80db
Siva Gangadhar G <119839967+sivagangadharthecoder@users.noreply.github.com>
2024-03-15 22:32:50 +0530

    import java.util.*;
    public class Index{
        public static void main(String[] args){
            Scanner sc = new Scanner(System.in);
            
            int size = sc.nextInt();
            int A[] = new int[size];
            for(int i=0; i<size; i++){
                A[i] = sc.nextInt();
            }
            
            int index = 0;
            for(int i=0; i<size; i++){
                if(A[i] % 2 != 0){
                    index = i;
                }
            }
            System.out.print(index);
        }
    }

---
+++ Last_odd_number_index_position.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Index{
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        
+        int size = sc.nextInt();
+        int A[] = new int[size];
+        for(int i=0; i<size; i++){
+            A[i] = sc.nextInt();
+        }
+        
+        int index = 0;
+        for(int i=0; i<size; i++){
+            if(A[i] % 2 != 0){
+                index = i;
+            }
+        }
+        System.out.print(index);
+    }
+}
